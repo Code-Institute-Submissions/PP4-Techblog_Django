76dbf530ff6d424beca33f964db1951d715086d2
Markp1312 <mpg.peeters3@gmail.com>
2022-01-01 14:08:22 +0000

    updated models.py

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 
 
 class Post(models.Model):
@@ -10,5 +11,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
+
+    
+    def get_absolute_url(self):
+        return reverse('home')
 
 # Create your models here.
