c5fbdcbc1926264c2811249eddcc71bdb5bd8054
Markp1312 <mpg.peeters3@gmail.com>
2022-01-01 13:54:47 +0000

    Added form to add_post and created view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 # from . import views
-from .views import HomePage, ArticleDetailView
+from .views import HomePage, ArticleDetailView, AddPostView
 
 urlpatterns = [
     path('', HomePage.as_view(), name="home"),
     path('article/<int:pk>', ArticleDetailView.as_view(), name='article-detail'),
+    path('add_post/', AddPostView.as_view(), name='add_post')
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
 
@@ -16,3 +16,8 @@ class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
 
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = '__all__'
