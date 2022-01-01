66b7608878110c2756e6a25268bcec244a609794
Markp1312 <mpg.peeters3@gmail.com>
2022-01-01 13:12:24 +0000

    Added title tag to data model

--- blog/models.py
+++ blog/models.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import User
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
+    title_tag = models.CharField(max_length=150, default="Mark Techblog")
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
 
