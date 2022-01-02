8a339183bc139f9fd6f36ae8e3f099a3a482f152
Markp1312 <mpg.peeters3@gmail.com>
2022-01-02 17:43:45 +0000

    Created registration templates and linked up in views/urls.py

--- subscribers/urls.py
+++ subscribers/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
-# from . import views
+from .views import UserRegisterView
 
 urlpatterns = [
-   
+   path('register/', UserRegisterView.as_view(), name='registration'),
 ]
