76e763d969dadc505459f6cc2b202a3f31485263
Markp1312 <mpg.peeters3@gmail.com>
2022-01-03 11:58:24 +0000

    Added category to model

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,15 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from datetime import datetime, date
 
+class Category(models.Model):
+    name = models.CharField(max_length=150)
+    
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('home')
+
 
 class Post(models.Model):
     title = models.CharField(max_length=150)
@@ -10,6 +19,7 @@ class Post(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
     publication_date = models.DateField(auto_now_add=True)
+    category = models.CharField(max_length=150, default='uncategorised')
 
     def __str__(self):
         return self.title + ' | ' + str(self.author)
