7921be7282b1c1aef4921e6fb5bb4ab6fbaf7ad8
Markp1312 <mpg.peeters3@gmail.com>
2022-01-03 12:17:54 +0000

    Add category choice in forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,16 +1,24 @@
 from django import forms
-from .models import Post
+from .models import Post, Category
+
+cats = Category.objects.all().values_list('name', 'name')
+
+cats_list = []
+
+for item in cats:
+    cats_list.append(item)
 
 
 class PostingForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'author', 'content')
+        fields = ('title', 'title_tag', 'author', 'category', 'content')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'author': forms.Select(attrs={'class': 'form-control'}),
+            'category': forms.Select(choices=cats_list, attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
 
