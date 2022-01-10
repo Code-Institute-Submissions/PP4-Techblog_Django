6d71748b7730995f967d21d7398bd74b404fa108
Markp1312 <mpg.peeters3@gmail.com>
2022-01-10 14:38:43 +0000

    Updated forms.py subscribers app

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -2,3 +2,12 @@ from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
 
+class RegistrationForm(UserCreationForm):
+    first_name = forms.CharField(max_length=20)
+    last_name = forms.CharField(max_length=20)
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        field = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+        
