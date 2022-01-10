9b58588e7669843b54afdb5586a182487d2a6a82
Markp1312 <mpg.peeters3@gmail.com>
2022-01-10 19:29:51 +0000

    Added snip to editform

--- blog/forms.py
+++ blog/forms.py
@@ -27,10 +27,11 @@ class PostingForm(forms.ModelForm):
 class EditForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ('title', 'title_tag', 'content')
+        fields = ('title', 'title_tag', 'content', 'snip')
 
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
+            'snip': forms.Textarea(attrs={'class': 'form-control'}),
         }
