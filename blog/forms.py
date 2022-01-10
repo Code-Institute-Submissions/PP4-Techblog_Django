f6e97129f9ad7ed2588cf1b048125bef9044a6ee
Markp1312 <mpg.peeters3@gmail.com>
2022-01-10 12:22:46 +0000

    Updated forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -17,7 +17,7 @@ class PostingForm(forms.ModelForm):
         widgets = {
             'title': forms.TextInput(attrs={'class': 'form-control'}),
             'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
-            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'peeters', 'type':'hidden'}),
+            'author': forms.TextInput(attrs={'class': 'form-control', 'value':'', 'id':'identifier', 'type':'hidden'}),
             'category': forms.Select(choices=cats_list, attrs={'class': 'form-control'}),
             'content': forms.Textarea(attrs={'class': 'form-control'}),
         }
