18336595d76ea2a0ccc8a2b983cf0e350563278e
Markp1312 <mpg.peeters3@gmail.com>
2022-01-10 14:56:00 +0000

    Updated forms.py subscriber app

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -3,11 +3,17 @@ from django.contrib.auth.models import User
 from django import forms
 
 class RegistrationForm(UserCreationForm):
-    first_name = forms.CharField(max_length=20)
-    last_name = forms.CharField(max_length=20)
-    email = forms.EmailField()
+    first_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
 
     class Meta:
         model = User
-        field = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
-        
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+
+        self.fields['username'].widget.attrs['class'] = 'form-control'
+        self.fields['password1'].widget.attrs['class'] = 'form-control'
+        self.fields['password2'].widget.attrs['class'] = 'form-control'
--- subscribers/views.py
+++ subscribers/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
+from .forms import RegistrationForm
 
 
 class UserRegisterView(generic.CreateView):
-    form_class = UserCreationForm
+    form_class = RegistrationForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
