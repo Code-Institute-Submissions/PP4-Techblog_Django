127bf0d6917ec7f19dc5b3e82c7297d1560546fc
Markp1312 <mpg.peeters3@gmail.com>
2022-01-11 11:47:16 +0000

    Updated forms.py with UpdateProfileForm

--- subscribers/forms.py
+++ subscribers/forms.py
@@ -17,3 +17,16 @@ class RegistrationForm(UserCreationForm):
         self.fields['username'].widget.attrs['class'] = 'form-control'
         self.fields['password1'].widget.attrs['class'] = 'form-control'
         self.fields['password2'].widget.attrs['class'] = 'form-control'
+
+
+class UpdateProfileForm(UserChangeForm):
+    first_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_name = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    username = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    last_login = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+    date_joined = forms.CharField(max_length=20, widget=forms.TextInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password', 'last_login', 'date_joined')
