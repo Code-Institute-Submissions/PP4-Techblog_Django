dc0a10eb58d87bade021586a302f8e966155f5d2
Markp1312 <mpg.peeters3@gmail.com>
2022-01-11 11:11:40 +0000

    Added update_profile html

--- subscribers/views.py
+++ subscribers/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import RegistrationForm
 
@@ -9,3 +9,8 @@ class UserRegisterView(generic.CreateView):
     form_class = RegistrationForm
     template_name = 'registration/registration.html'
     success_url = reverse_lazy('login')
+
+class UserUpdateView(generic.CreateView):
+    form_class = UserChangeForm
+    template_name = 'registration/update_profile.html'
+    success_url = reverse_lazy('home')
