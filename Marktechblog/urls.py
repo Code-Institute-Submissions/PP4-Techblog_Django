4e9a775ec9cbe33b9e6596a99d7cb7ef4af84eae
Markp1312 <mpg.peeters3@gmail.com>
2022-01-13 14:01:37 +0000

    Added Media URL to URLS.py

--- Marktechblog/urls.py
+++ Marktechblog/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
     path('subscribers/', include('django.contrib.auth.urls')),
     path('subscribers/', include('subscribers.urls')),
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
