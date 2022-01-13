e96eb4f0480b5bdcdd6632ec5e90a71ad77f93fa
Markp1312 <mpg.peeters3@gmail.com>
2022-01-13 10:58:53 +0000

    Added Userprofile to Data Model

--- blog/admin.py
+++ blog/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Post, Category
+from .models import Post, Category, UserProfile
 
 admin.site.register(Post)
 admin.site.register(Category)
+admin.site.register(UserProfile)
 
 # Register your models here.
--- blog/models.py
+++ blog/models.py
@@ -13,6 +13,12 @@ class Category(models.Model):
     def get_absolute_url(self):
         return reverse('home')
 
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
 
 class Post(models.Model):
     title = models.CharField(max_length=50)
