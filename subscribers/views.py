729814328a6768bcdb1fd03916843dd37339e697
Markp1312 <mpg.peeters3@gmail.com>
2022-01-17 12:56:37 +0000

    updated ProfilePageView

--- subscribers/views.py
+++ subscribers/views.py
@@ -1,14 +1,22 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from django.views.generic import DetailView
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 from django.urls import reverse_lazy
 from .forms import RegistrationForm, UpdateProfileForm
-
+from blog.models import UserProfile
 
 class ProfilePageView(DetailView):
-    model = Profile
+    model = UserProfile
     template_name = 'registration/user_profile.html'
+    
+    def get_context_data(self, *args, **kwargs):
+        users = UserProfile.objects.all()
+        context = super(ProfilePageView, self).get_context_data(*args, **kwargs)
+        
+        page_user = get_object_or_404(UserProfile, id=self.kwargs['pk'])
+        context["page_user"] = page_user
+        return context
 
 
 
