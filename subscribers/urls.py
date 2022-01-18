6319169c47e8db21e5a625c7f2452c0ae616bf93
Markp1312 <mpg.peeters3@gmail.com>
2022-01-18 11:27:00 +0000

    Added CreateProfilePage to urls

--- subscribers/urls.py
+++ subscribers/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import UserRegisterView, UserUpdateView, ProfilePageView, EditProfilePageView
+from .views import UserRegisterView, UserUpdateView, ProfilePageView, EditProfilePageView, CreateProfilePageView
 
 urlpatterns = [
    path('register/', UserRegisterView.as_view(), name='registration'),
    path('update_profile/', UserUpdateView.as_view(), name='update_profile'),
    path('<int:pk>/profile/', ProfilePageView.as_view(), name='show_profile_page'),
    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
+   path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
 ]
