71d07b440b8b22d27438d0550f5277413549810a
Markp1312 <mpg.peeters3@gmail.com>
2022-01-19 15:13:04 +0000

    Added password to urls.py under subscriber

--- subscribers/urls.py
+++ subscribers/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import UserRegisterView, UserUpdateView, ProfilePageView, EditProfilePageView, CreateProfilePageView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
    path('register/', UserRegisterView.as_view(), name='registration'),
@@ -7,4 +8,5 @@ urlpatterns = [
    path('<int:pk>/profile/', ProfilePageView.as_view(), name='show_profile_page'),
    path('<int:pk>/edit_profile_page/', EditProfilePageView.as_view(), name='edit_profile_page'),
    path('create_profile_page/', CreateProfilePageView.as_view(), name='create_profile_page'),
+   path('password/', auth_views.PasswordChangeView.as_view(template_name='registration/change-password.html')),
 ]
