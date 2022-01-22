5bfc286ffdeddf44a2a912aad41b07df04430841
Markp1312 <mpg.peeters3@gmail.com>
2022-01-22 11:43:17 +0000

    Final Commit

--- Marktechblog/settings.py
+++ Marktechblog/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "pp4-marktechblog.herokuapp.com/"]
+ALLOWED_HOSTS = ["localhost", "pp4-marktechblog.herokuapp.com"]
 
 
 # Application definition
